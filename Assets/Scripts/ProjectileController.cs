1c0c0770982205471ae87aafeeb2a1cc65cb9ed3
Pojemnik <andriej194@gmail.com>
2021-04-05 21:29:18 +0200

    Fixed projectile trails after hitting an object

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -6,9 +6,20 @@ public class ProjectileController : MonoBehaviour
 {
     public GameObject hitEffectPrefab;
 
+    private new Rigidbody rigidbody;
+    private new Collider collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(Instantiate(hitEffectPrefab, collision.GetContact(0).point, Quaternion.Euler(collision.GetContact(0).normal)), 1);
-        Destroy(gameObject);
+        Destroy(rigidbody);
+        Destroy(collider);
+        Destroy(gameObject, 1);
     }
 }
