0de757ee62497d0aed39f25d102ee218bf7d2cec
Pojemnik <andriej194@gmail.com>
2021-06-30 20:39:30 +0200

    Added game over screen

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private string startSceneName;
+
+    [Header("Game over screen")]
+    [SerializeField]
+    private GameObject gameOverScreen;
+    [SerializeField]
+    private float gameOverScreenDisplayTime;
+
+    public void OnPlayerDeath()
+    {
+        StartCoroutine(DisplayGameOverScreen(gameOverScreenDisplayTime));
+    }
+
+    private IEnumerator DisplayGameOverScreen(float time)
+    {
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(time);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(startSceneName);
+        gameOverScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
