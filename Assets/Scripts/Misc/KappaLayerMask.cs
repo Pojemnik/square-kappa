bd04bfd8480defc03140a16129e7a7ea537e28c2
Pojemnik <andriej194@gmail.com>
2021-08-24 11:17:02 +0200

    Make enemies see through glass

--- Assets/Scripts/Misc/KappaLayerMask.cs
+++ Assets/Scripts/Misc/KappaLayerMask.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public static class KappaLayerMask
 {
-    private static int enemyVisionMask = ~LayerMask.GetMask("Enemy", "PlayerProjectile", "EnemyProjectile", "PlayerEnvironmentalCollision", "Objectives");
-    private static int enemyMovementMask = ~LayerMask.GetMask("Enemy", "PlayerProjectile", "EnemyProjectile", "PlayerEnvironmentalCollision", "Objectives");
-    private static int playerVisionMask = ~LayerMask.GetMask("Player", "PlayerProjectile", "EnemyProjectile", "PlayerEnvironmentalCollision", "Objectives", "Transparent");
+    private static int enemyVisionMask = ~LayerMask.GetMask("Enemy", "PlayerProjectile", "EnemyProjectile",
+                                                            "PlayerEnvironmentalCollision", "Objectives", "Transparent");
+    private static int enemyMovementMask = ~LayerMask.GetMask("Enemy", "PlayerProjectile", "EnemyProjectile",
+                                                              "PlayerEnvironmentalCollision", "Objectives", "Transparent");
+    private static int playerVisionMask = ~LayerMask.GetMask("Player", "PlayerProjectile", "EnemyProjectile",
+                                                             "PlayerEnvironmentalCollision", "Objectives", "Transparent");
 
     public static int EnemyVisionMask { get => enemyVisionMask; }
     public static int EnemyMovementMask { get => enemyMovementMask; }
