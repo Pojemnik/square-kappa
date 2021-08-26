0ef8b8674ef97afaf2e84704ef1270a183590da4
Pojemnik <andriej194@gmail.com>
2021-08-26 13:22:54 +0200

    Make objective zones with more than one trigger work

--- Assets/Scripts/Logic/ObjectiveZone.cs
+++ Assets/Scripts/Logic/ObjectiveZone.cs
@@ -8,34 +8,55 @@ public class ObjectiveZone : Objective
     [SerializeField]
     private bool completedWhenLeft;
 
+    private int triggersCount;
+    private int currentlyCheckedTriggers;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        triggersCount = GetComponents<Collider>().Length;
+        currentlyCheckedTriggers = 0;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("PlayerTrigger"))
         {
-            print(string.Format("Objective zone {0} entered", gameObject.name));
-            if (completedWhenLeft)
+            currentlyCheckedTriggers++;
+            Debug.LogFormat("Objective zone {0} entered", gameObject.name);
+            Debug.LogFormat("Checked triggers count: {0}", currentlyCheckedTriggers);
+            if (currentlyCheckedTriggers == 1)
             {
-                Uncomplete();
-            }
-            else
-            {
-                Complete();
+                if (completedWhenLeft)
+                {
+                    Uncomplete();
+                }
+                else
+                {
+                    Complete();
+                }
+
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerTrigger"))
         {
-            print(string.Format("Objective zone {0} left", gameObject.name));
-            if (completedWhenLeft)
+            currentlyCheckedTriggers--;
+            Debug.LogFormat("Objective zone {0} left", gameObject.name);
+            Debug.LogFormat("Checked triggers count: {0}", currentlyCheckedTriggers);
+            if (currentlyCheckedTriggers == 0)
             {
-                Complete();
-            }
-            else
-            {
-                Uncomplete();
+                if (completedWhenLeft)
+                {
+                    Complete();
+                }
+                else
+                {
+                    Uncomplete();
+                }
             }
         }
     }
