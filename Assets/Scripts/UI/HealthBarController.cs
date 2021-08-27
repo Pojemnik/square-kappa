80c42f97f0c60326a07519f57df94602b4d0ce27
Pojemnik <andriej194@gmail.com>
2021-08-27 09:54:20 +0200

    Implement HealthBarController

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    [Tooltip("Width of bar when health is 0")]
+    [SerializeField]
+    private float minWidth;
+    [Tooltip("Width of bar when health is 100")]
+    [SerializeField]
+    private float maxWidth;
+
+    private RectTransform rect;
+
+    public void OnPlayerHealthChange(int value)
+    {
+        if (value < 0)
+        {
+            Debug.LogError("Health value less than 0");
+            return;
+        }
+        float hpPercent = value / 100f;
+        float width = Mathf.Lerp(minWidth, maxWidth, hpPercent);
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+    }
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxWidth);
+    }
+
+}
