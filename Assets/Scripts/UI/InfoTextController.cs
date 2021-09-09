56aa0c7735a72f475a00fc2319c14ff792a6a207
Pojemnik <andriej194@gmail.com>
2021-09-09 17:16:07 +0200

    Add InfoText prototype

---
+++ Assets/Scripts/UI/InfoTextController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoTextController : MonoBehaviour
+{
+    [SerializeField]
+    private string cursor;
+    [SerializeField]
+    private float letterTypeDuration;
+    [SerializeField]
+    private float cursorVisibleTime;
+    [SerializeField]
+    private float cursorInvisibleTime;
+
+    private TMPro.TextMeshProUGUI textMesh;
+    private string textToType;
+    private System.CharEnumerator enumerator;
+    private string typedText;
+    private bool cursorState;
+    private bool typing;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TMPro.TextMeshProUGUI>();
+        TypeText("This is a test message printed on screen by InfoTextController");
+    }
+
+    public void TypeText(string text)
+    {
+        textToType = text;
+        enumerator = textToType.GetEnumerator();
+        textMesh.text = cursor;
+        typedText = "";
+        typing = true;
+        cursorState = true;
+        StartCoroutine(TypeTextCoroutine());
+        StartCoroutine(CurosrBlinkCoroutine());
+    }
+
+    private IEnumerator TypeTextCoroutine()
+    {
+        while (enumerator.MoveNext())
+        {
+            yield return new WaitForSecondsRealtime(letterTypeDuration);
+            typedText += enumerator.Current;
+            UpdateText();
+        }
+        textMesh.text = textToType;
+        typing = false;
+        yield return new WaitForSecondsRealtime(letterTypeDuration);
+        OnTypingEnd();
+    }
+
+    private IEnumerator CurosrBlinkCoroutine()
+    {
+        while (typing)
+        {
+            if (cursorState)
+            {
+                yield return new WaitForSecondsRealtime(cursorVisibleTime);
+            }
+            else
+            {
+                yield return new WaitForSecondsRealtime(cursorInvisibleTime);
+            }
+            cursorState = !cursorState;
+            UpdateText();
+        }
+        cursorState = false;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        textMesh.text = typedText + (cursorState ? cursor : "");
+    }
+
+    private void OnTypingEnd()
+    {
+        textMesh.enabled = false;
+    }
+}
