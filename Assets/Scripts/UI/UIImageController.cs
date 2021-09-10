2d9a0501f9de3f7c8e365758e5ed4deceb401582
Pojemnik <andriej194@gmail.com>
2021-09-10 22:17:25 +0200

    Fix images localization

--- Assets/Scripts/UI/UIImageController.cs
+++ Assets/Scripts/UI/UIImageController.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 
 public class UIImageController : MonoBehaviour
 {
-    private UnityEngine.UI.Image image;
-
+    [SerializeField]
+    private GameObject image;
     [SerializeField]
     private float displayTime;
-
+    [SerializeField]
+    private bool localizedImage;
 
     private void Awake()
     {
-        image = GetComponent<UnityEngine.UI.Image>();
-        image.enabled = false;
+        image.SetActive(false);
     }
 
     public void ShowScreenForTime()
     {
-        image.enabled = true;
+        image.SetActive(true);
         StartCoroutine(WaitAndHideImage(displayTime));
     }
 
     public void ShowScreen()
     {
-        image.enabled = true;
+        image.SetActive(true);
+        if(localizedImage)
+        {
+            var localizer = image.GetComponent<UnityEngine.Localization.PropertyVariants.GameObjectLocalizer>();
+            localizer.ApplyLocaleVariant(UnityEngine.Localization.Settings.LocalizationSettings.SelectedLocale);
+        }
     }
 
     public void HideScreen()
     {
-        image.enabled = false;
+        image.SetActive(false);
     }
 
     private IEnumerator WaitAndHideImage(float time)
