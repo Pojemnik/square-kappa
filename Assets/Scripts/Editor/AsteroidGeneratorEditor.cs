fa9b4a55dc65f47b2839b9e10829d47bcdebb90b
Pojemnik <andriej194@gmail.com>
2021-10-05 12:03:29 +0200

    Update asteroid generator

--- Assets/Scripts/Editor/AsteroidGeneratorEditor.cs
+++ Assets/Scripts/Editor/AsteroidGeneratorEditor.cs
@@ -6,30 +6,23 @@ using UnityEditor;
 [CustomEditor(typeof(AsteroidGenerator))]
 public class AsteroidGeneratorEditor : Editor
 {
-    private SerializedProperty noiseMultipler;
-    private SerializedProperty baseHeight;
-    private SerializedProperty offset;
-    private SerializedProperty positionScale;
+    private SerializedProperty meshSize;
+    private SerializedProperty noise;
 
     private AsteroidGenerator sphere;
 
     void OnEnable()
     {
-        noiseMultipler = serializedObject.FindProperty("noiseMultipler");
-        baseHeight = serializedObject.FindProperty("baseHeight");
-        offset = serializedObject.FindProperty("offset");
-        positionScale = serializedObject.FindProperty("positionScale");
+        noise = serializedObject.FindProperty("noiseLayers");
+        meshSize = serializedObject.FindProperty("meshSize");
         sphere = (AsteroidGenerator)target;
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        EditorGUILayout.Slider(noiseMultipler, 0, 10);
-        EditorGUILayout.Slider(baseHeight, 0, 1);
-        EditorGUILayout.PropertyField(offset);
-        EditorGUILayout.Slider(positionScale, 0, 5);
-        sphere.noiseType = (AsteroidGenerator.NoiseType)EditorGUILayout.EnumPopup("Noise type", sphere.noiseType);
+        EditorGUILayout.IntSlider(meshSize, 3, 5);
+        EditorGUILayout.PropertyField(noise);
         serializedObject.ApplyModifiedProperties();
         if (GUILayout.Button("Generate"))
         {
--- Assets/Tools/AsteroidGenerator.cs
+++ Assets/Tools/AsteroidGenerator.cs
@@ -8,12 +8,54 @@ using Unity.Mathematics;
 [RequireComponent(typeof(MeshRenderer))]
 public class AsteroidGenerator : MonoBehaviour
 {
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled;
+        public float noiseMultipler;
+        public float baseHeight;
+        public Vector3 offset;
+        public float positionScale;
+        public enum NoiseType
+        {
+            Perlin,
+            Simplex,
+            Cellular3,
+            Cellular2
+        }
+
+        public NoiseType noiseType;
+
+        private Dictionary<NoiseType, System.Func<Vector3, float>> noiseFunctions;
+
+        private void GetNoiseFunctions()
+        {
+            noiseFunctions = new Dictionary<NoiseType, System.Func<Vector3, float>>
+            {
+                [NoiseType.Perlin] = (pos) => Unity.Mathematics.noise.cnoise(pos),
+                [NoiseType.Simplex] = (pos) => Unity.Mathematics.noise.snoise(pos),
+                [NoiseType.Cellular3] = (pos) => Unity.Mathematics.noise.cellular(pos).y,
+                [NoiseType.Cellular2] = (pos) => Unity.Mathematics.noise.cellular2x2x2(pos).x
+            };
+        }
+
+        public float GetVertexHeight(Vector3 vertex)
+        {
+            if(!enabled)
+            {
+                return 1;
+            }
+            if(noiseFunctions == null)
+            {
+                GetNoiseFunctions();
+            }
+            Vector3 pos = vertex * positionScale + offset;
+            return Mathf.Clamp01(noiseFunctions[noiseType](pos) / 2 + 0.5f) * noiseMultipler + baseHeight;
+        }
+    }
     public int meshSize;
 
-    public float noiseMultipler;
-    public float baseHeight;
-    public Vector3 offset;
-    public float positionScale;
+    public List<NoiseLayer> noiseLayers;
 
     public enum CraterOverlapMode
     {
@@ -24,38 +66,11 @@ public class AsteroidGenerator : MonoBehaviour
 
     public CraterOverlapMode craterOverlap;
 
-    public enum NoiseType
-    {
-        Perlin,
-        Simplex,
-        Cellular3,
-        Cellular2
-    }
-
-    public NoiseType noiseType;
-
     private MeshFilter filter;
     private Mesh mesh;
 
-    private Dictionary<NoiseType, System.Func<Vector3, float>> noiseFunctions;
-
-    private void GetNoiseFunctions()
-    {
-        noiseFunctions = new Dictionary<NoiseType, System.Func<Vector3, float>>
-        {
-            [NoiseType.Perlin] = (pos) => noise.cnoise(pos),
-            [NoiseType.Simplex] = (pos) => noise.snoise(pos),
-            [NoiseType.Cellular3] = (pos) => noise.cellular(pos).y,
-            [NoiseType.Cellular2] = (pos) => noise.cellular2x2x2(pos).x
-        };
-    }
-
     public void Generate()
     {
-        if(noiseFunctions == null)
-        {
-            GetNoiseFunctions();
-        }
         filter = GetComponent<MeshFilter>();
         mesh = filter.sharedMesh = new Mesh();
         Icosphere.Create(ref mesh, meshSize);
@@ -69,11 +84,31 @@ public class AsteroidGenerator : MonoBehaviour
         for (int i = 0; i < vertices.Length; i++)
         {
             float finalHeight = 1;
-            Vector3 pos = vertices[i] * positionScale + offset;
-            finalHeight = Mathf.Clamp01(noiseFunctions[noiseType](pos) / 2 + 0.5f) * noiseMultipler + baseHeight;
+            foreach (NoiseLayer layer in noiseLayers)
+            {
+                finalHeight *= layer.GetVertexHeight(vertices[i]);
+            }
             vertices[i] *= finalHeight;
         }
         mesh.vertices = vertices;
+    }
+
+    private float SmoothMin(float a, float b, float k)
+    {
+        float h = Mathf.Clamp01((b - a + k) / (2 * k));
+        return a * h + b * (1 - h) - k * h * (1 - h);
+    }
+
+    private Vector2 MapVertex(Vector3 vertex)
+    {
+        Vector2 result;
+        result.y = Mathf.Asin(vertex.y) / Mathf.PI + 0.5F;
+        result.x = Mathf.Atan2(vertex.x, vertex.z) / (Mathf.PI * -2);
+        if (result.x < 0)
+        {
+            result.x += 1;
+        }
+        return result;
     }
 
     private void RecalculateMesh()
