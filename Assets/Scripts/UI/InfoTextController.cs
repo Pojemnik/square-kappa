30612ddb88c93a9794e349902d4f33ad984119f7
Pojemnik <andriej194@gmail.com>
2021-10-16 17:13:13 +0200

    Fix odd line breaking when typing text on screen

--- Assets/Scripts/UI/InfoTextController.cs
+++ Assets/Scripts/UI/InfoTextController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class InfoTextController : MonoBehaviour
 {
-    [SerializeField]
-    private string cursor;
-    [SerializeField]
-    private float letterTypeDuration;
+    [Header("Cursor settings")]
     [SerializeField]
     private float cursorVisibleTime;
     [SerializeField]
     private float cursorInvisibleTime;
+    [SerializeField]
+    private GameObject cursorPrefab;
+    [SerializeField]
+    private Vector2 offset;
+
+    [Space]
+    [SerializeField]
+    private float letterTypeDuration;
     [HideInInspector]
     public event System.EventHandler displayEndEvent;
 
@@ -24,20 +29,28 @@ public class InfoTextController : MonoBehaviour
     private float endDelay;
     private bool paused;
     private (Coroutine type, Coroutine blink) coroutines;
+    private GameObject cursor;
 
     private void Awake()
     {
         textMesh = GetComponent<TMPro.TextMeshProUGUI>();
         paused = false;
         EventManager.Instance.AddListener("Unpause", ContinueTyping);
+        cursor = Instantiate(cursorPrefab, transform);
+        cursor.SetActive(false);
     }
 
     public void TypeText(string text, float hideDelay)
     {
         StopTyping();
+        var info = textMesh.GetTextInfo(text);
+        for (int i = 0; i < info.lineCount; i++)
+        {
+            TMPro.TMP_LineInfo line = info.lineInfo[i];
+            text = text.Remove(line.lastCharacterIndex, 1).Insert(line.lastCharacterIndex, "\n");
+        }
         textToType = text;
         enumerator = textToType.GetEnumerator();
-        textMesh.text = cursor;
         typedText = "";
         typing = true;
         cursorState = true;
@@ -49,7 +62,7 @@ public class InfoTextController : MonoBehaviour
 
     public void StopTyping()
     {
-        if(!typing)
+        if (!typing)
         {
             return;
         }
@@ -117,7 +130,13 @@ public class InfoTextController : MonoBehaviour
 
     private void UpdateText()
     {
-        textMesh.text = typedText + (cursorState ? cursor : "");
+        string text = typedText;// + (cursorState ? cursorString : "");
+        textMesh.text = text;
+        var info = textMesh.GetTextInfo(text);
+        float x = info.lineInfo[info.lineCount - 1].length;
+        float y = -info.lineInfo[0].lineHeight * info.lineCount;
+        cursor.transform.localPosition = new Vector2(x, y) + offset;
+        cursor.SetActive(cursorState);
     }
 
     private void OnTypingEnd()
