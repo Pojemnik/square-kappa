f2ed4471566f29b2f536c78c33e08ec71f8db069
Pojemnik <andriej194@gmail.com>
2021-11-09 20:08:37 +0100

    Fix sounds playing during pause, fix volume scale

--- Assets/Scripts/Logic/Managers/SoundManager.cs
+++ Assets/Scripts/Logic/Managers/SoundManager.cs
@@ -16,11 +16,36 @@ public class SoundManager : Singleton<SoundManager>
     {
         ChangeEffectVolume(SettingsManager.Instance.SoundVolume.Value);
         SettingsManager.Instance.SoundVolume.ValueChanged += (_, v) => ChangeEffectVolume(v);
+        EventManager.Instance.AddListener("Pause", () => PauseSounds());
+        EventManager.Instance.AddListener("StopTime", () => PauseSounds());
+        EventManager.Instance.AddListener("Victory", () => PauseSounds());
+        EventManager.Instance.AddListener("PlayerDeath", () => PauseSounds());
+        EventManager.Instance.AddListener("Unpause", () => UnpauseSounds());
+        EventManager.Instance.AddListener("ResetTimescale", () => UnpauseSounds());
+        EventManager.Instance.AddListener("GameReloaded", () => UnpauseSounds());
+    }
+
+    private void PauseSounds()
+    {
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+        foreach(AudioSource source in audioSources)
+        {
+            source.Pause();
+        }
+    }
+
+    private void UnpauseSounds()
+    {
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource source in audioSources)
+        {
+            source.UnPause();
+        }
     }
 
     private float MapFloatToVolume(float value)
     {
-        return Mathf.Lerp(-80, 20, value);
+        return Mathf.Log10(value) * 20;
     }
 
     public void ChangeEffectVolume(float value)
