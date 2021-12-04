80c2bbcd0632b7aa9002c18c631fd99385ab00be
Pojemnik <andriej194@gmail.com>
2021-12-04 14:22:19 +0100

    Fix turret shooting

--- Assets/Scripts/Units/Turret/TurretMovement.cs
+++ Assets/Scripts/Units/Turret/TurretMovement.cs
@@ -9,12 +9,27 @@ public class TurretMovement : UnitMovement
     public override bool UseDrag { get; set; }
     public override bool IsRotating { get; protected set; }
 
-    //[SerializeField]
-    //private Transform turretHead;
-    //[SerializeField]
-    //private Transform turretGun;
     [SerializeField]
-    private Turret turret;
+    private Transform turretGun;
+    [SerializeField]
+    private Transform turretHead;
+    [SerializeField]
+    private float pitchSpeed;
+    [SerializeField]
+    private float yawSpeed;
+    [SerializeField]
+    private float pitchLimit;
+    [SerializeField]
+    private float yawLimit;
+
+    private Quaternion turretHeadStartRotation;
+    private Quaternion turretGunStartRotation;
+
+    protected void Start()
+    {
+        turretGunStartRotation = turretGun.localRotation;
+        turretHeadStartRotation = turretHead.localRotation;
+    }
 
     public override void EnableStopMode()
     {
@@ -63,31 +78,64 @@ public class TurretMovement : UnitMovement
 
     public override void SetRotationImmediately(Vector3 direction)
     {
-        turret.target = direction + transform.position;
-        Debug.Log(direction);
-        //Vector3 d = transform.InverseTransformDirection(direction);
-        //Vector3 xRotDir = transform.TransformDirection(new Vector3(0, d.y, 0));
-        //Vector3 yRotDir = transform.TransformDirection(new Vector3(d.x, 0, 0));
-        //Quaternion xRot = Quaternion.LookRotation(xRotDir);
-        //Quaternion yRot = Quaternion.LookRotation(yRotDir);
-        //turretGun.rotation = xRot;
-        //turretHead.rotation = yRot;
-        //Debug.Log("XD");
-        //if (cameraAiming)
-        //{
-        //    owner.TowardsTarget = Quaternion.LookRotation(firstPresonCamera.transform.forward, firstPresonCamera.transform.up);
-        //}
-        //else
-        //{
-        //    if (direction != Vector3.zero)
-        //    {
-        //        owner.TowardsTarget = Quaternion.LookRotation(direction);
-        //    }
-        //}
+        Vector3 target = direction + transform.position;
+        float angle;
+        Vector3 targetRelative;
+        Quaternion targetRotation;
+        if (turretHead && (yawLimit != 0f))
+        {
+            targetRelative = turretHead.InverseTransformPoint(target);
+            angle = Mathf.Atan2(targetRelative.x, targetRelative.z) * Mathf.Rad2Deg;
+            if (angle >= 180f)
+            {
+                angle = 180f - angle;
+            }
+            if (angle <= -180f)
+            {
+                angle = -180f + angle;
+            }
+            targetRotation = turretHead.rotation * Quaternion.Euler(0f, Mathf.Clamp(angle, -yawSpeed * Time.deltaTime, yawSpeed * Time.deltaTime), 0f);
+            if ((yawLimit < 360f) && (yawLimit > 0f))
+            {
+                turretHead.rotation = Quaternion.RotateTowards(turretHead.parent.rotation * turretHeadStartRotation, targetRotation, yawLimit);
+            }
+            else
+            {
+                turretHead.rotation = targetRotation;
+            }
+        }
+        if (turretGun && (pitchLimit != 0f))
+        {
+            targetRelative = turretGun.InverseTransformPoint(target);
+            angle = -Mathf.Atan2(targetRelative.y, targetRelative.z) * Mathf.Rad2Deg;
+            if (angle >= 180f)
+            {
+                angle = 180f - angle;
+            }
+            if (angle <= -180f)
+            {
+                angle = -180f + angle;
+            }
+            targetRotation = turretGun.rotation * Quaternion.Euler(Mathf.Clamp(angle, -pitchSpeed * Time.deltaTime, pitchSpeed * Time.deltaTime), 0f, 0f);
+            if ((pitchLimit < 360f) && (pitchLimit > 0f))
+            {
+                turretGun.rotation = Quaternion.RotateTowards(turretGun.parent.rotation * turretGunStartRotation, targetRotation, pitchLimit);
+            }
+            else
+            {
+                turretGun.rotation = targetRotation;
+            }
+        }
+        owner.TowardsTarget = turretGun.rotation;
     }
 
     public override void SetTargetRotation(Quaternion direction)
     {
-        //throw new System.NotImplementedException();
+        SetTargetRotation(direction * Vector3.forward);
+    }
+
+    public override void SetTargetRotation(Vector3 direction)
+    {
+        SetRotationImmediately(direction);
     }
 }
