97af1b8fb3c40cb712dc119a405fcda610d6660e
Pojemnik <andriej194@gmail.com>
2022-02-19 13:57:50 +0100

    Fix regular objective

--- Assets/Scripts/Logic/Missions/Objectives/Objective.cs
+++ Assets/Scripts/Logic/Missions/Objectives/Objective.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public abstract class Objective : MonoBehaviour
+public class Objective : MonoBehaviour
 {
     private static int idCounter = 0;
     private int id;
